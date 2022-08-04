28fb03c1140c35358f79995334c4eefc2298be5f
Aruming <0430kar@naver.com>
2022-08-04 14:30:32 +0900

    이전글, 다음글에서 블라인드 된 글 거르기

--- src/main/java/com/ll/exam/article/repository/ArticleRepository.java
+++ src/main/java/com/ll/exam/article/repository/ArticleRepository.java
@@ -85,6 +85,7 @@ public class ArticleRepository {
                 .append("SELECT *")
                 .append("FROM article")
                 .append("WHERE id < ?", id)
+                .append("AND isBlind = 0")
                 .append("ORDER BY id DESC")
                 .append("LIMIT 1");
         return sql.selectRow(ArticleDto.class);
@@ -96,6 +97,7 @@ public class ArticleRepository {
                 .append("SELECT *")
                 .append("FROM article")
                 .append("WHERE id > ?", id)
+                .append("AND isBlind = 0")
                 .append("ORDER BY id ASC")
                 .append("LIMIT 1");
         return sql.selectRow(ArticleDto.class);
