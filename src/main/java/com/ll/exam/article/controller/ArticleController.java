06db9a5c070b88d297fe132c6d9010787b19d913
Aruming <0430kar@naver.com>
2022-08-04 14:58:31 +0900

    게시물 작성 폼 구현

--- src/main/java/com/ll/exam/article/controller/ArticleController.java
+++ src/main/java/com/ll/exam/article/controller/ArticleController.java
@@ -24,4 +24,9 @@ public class ArticleController {
         rq.setAttr("articles", articleDtos);
         rq.view("usr/article/list");
     }
+
+    @GetMapping("/usr/article/write")
+    public void showWrite(Rq rq) {
+        rq.view("usr/article/write");
+    }
 }
